66b8192738a6185f49d8efafbce430ddda01cf98
TOMASZ\tomas <tomasz1lesniak@gmail.com>
2024-07-12 16:24:44 +0200

    add create trip view

--- trip_calculator/trip_calculator/views.py
+++ trip_calculator/trip_calculator/views.py
@@ -6,6 +6,11 @@ trip = {"name": "Rumunia","all_person": [{'name':'Tomasz', 'lastname':'Leśniak'
 login = {"error": False}
 background ={'img_url':'https://images.unsplash.com/photo-1500964757637-c85e8a162699?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MzIyMjJ8MHwxfHNlYXJjaHwxfHxyYW5kb20lMjBuYXR1cmFsJTIwdmlld3xlbnwwfHx8fDE3MjA3MjU1MDV8MA&ixlib=rb-4.0.3&q=80&w=1080' }
 
+menu = {"current_page": 'Create new trip'}
+person= {"frend_gr1":[{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"}],
+"frend_gr2":[{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"}],
+"frend_gr3":[{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"}]
+         }
 
 def login_page_view(request):
     return render(request, 'trip_calculator/login_page/login.html', {'login': login, 'background': background})
@@ -17,3 +22,6 @@ def error_login_page_view(request):
 
 def add_expense_view(request):
     pass
+
+def create_trip_view(request):
+    return render(request, 'trip_calculator/create_trip.html', {'menu': menu, 'person': person})
