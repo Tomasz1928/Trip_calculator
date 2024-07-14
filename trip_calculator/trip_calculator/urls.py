41d3f66be7632e519a40dac290e9be1084879653
TOMASZ\tomas <tomasz1lesniak@gmail.com>
2024-07-14 09:46:33 +0200

    remove footer, add background, unification styles

--- trip_calculator/trip_calculator/urls.py
+++ trip_calculator/trip_calculator/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('login/', views.login_page_view, name='login_view'),
     path('error_login/', views.error_login_page_view, name='error_login_view'),
     path('create_trip/', views.create_trip_view, name='create_trip_view'),
-    # path('home/', views.home_view, name='home_view'),
+    path('invite_friend/', views.invite_friend_view, name='invite_friend_view'),
     # path('add_expense/', views.add_expense_view, name='add_expense_view')
 ]
--- trip_calculator/trip_calculator/views.py
+++ trip_calculator/trip_calculator/views.py
@@ -3,27 +3,32 @@ from django.shortcuts import render, redirect
 # Create your views here.
 person = {"name": 'Tomasz'}
 trip = {"name": "Rumunia","all_person": [{'name':'Tomasz', 'lastname':'Leśniak'},{'name':'Robert', 'lastname':'Kromka'}, {'name':'ff', 'lastname':'tt'},] }
-login = {"error": False}
 background ={'img_url':'https://images.unsplash.com/photo-1500964757637-c85e8a162699?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MzIyMjJ8MHwxfHNlYXJjaHwxfHxyYW5kb20lMjBuYXR1cmFsJTIwdmlld3xlbnwwfHx8fDE3MjA3MjU1MDV8MA&ixlib=rb-4.0.3&q=80&w=1080' }
 
-menu = {"current_page": 'Create new trip'}
 person= {"frend_gr1":[{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"}],
 "frend_gr3":[{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"},{"name":"Test1", "lastname":"test2"}]
          }
 
+
 def login_page_view(request):
-    return render(request, 'trip_calculator/login_page/login.html', {'login': login, 'background': background})
+    login = {"error": False}
+    return render(request, 'trip_calculator/login.html', {'login': login, 'background': background})
+
 
 
 def error_login_page_view(request):
-    return render(request, 'trip_calculator/login_page/login.html', {'login': login, 'background': background})
+    login = {"error": True}
+    return render(request, 'trip_calculator/login.html', {'login': login, 'background': background})
 
 
-def add_expense_view(request):
-    pass
 
 def create_trip_view(request):
+    menu = {"current_page": 'Create new trip'}
     if request.method == 'POST':
         return redirect('login_view')
+    return render(request, 'trip_calculator/create_trip.html', {'menu': menu, 'person': person, 'background': background})
 
-    return render(request, 'trip_calculator/create_trip.html', {'menu': menu, 'person': person})
+
+def invite_friend_view(request):
+    menu = {"current_page": 'Invite new friend'}
+    return render(request, 'trip_calculator/addFriends.html', {'menu': menu, 'background': background})
