af7a85f0585d18009f94da708532268091864b98
TOMASZ\tomas <tomasz1lesniak@gmail.com>
2024-07-16 21:20:09 +0200

    add new wiev

--- trip_calculator/trip_calculator/urls.py
+++ trip_calculator/trip_calculator/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('registration/', views.registration_view, name='registration_view'),
     path('create_trip/', views.create_trip_view, name='create_trip_view'),
     path('invite_friend/', views.invite_friend_view, name='invite_friend_view'),
-
+    path('add_cost/', views.add_cost_view, name='add_cost_view'),
+    path('home/', views.home_view, name='home_view'),
     # path('add_expense/', views.add_expense_view, name='add_expense_view')
 ]
